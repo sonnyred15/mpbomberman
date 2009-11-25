264a01b01b74db4c70b58b791f3781119518eeaa
maverick89@bk.ru <maverick89@bk.ru@c20015b8-d073-11de-97bf-6350044336de>
2009-11-25 00:10:41 +0000

    Add icons

--- branches/explList/src/org/amse/bomberman/client/view/MapJFrame.java
+++ branches/explList/src/org/amse/bomberman/client/view/MapJFrame.java
@@ -5,12 +5,12 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.util.ArrayList;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.SwingConstants;
-import javax.swing.border.LineBorder;
+import org.amse.bomberman.client.Main;
 import org.amse.bomberman.client.model.IModel;
 import org.amse.bomberman.client.model.BombMap;
 import org.amse.bomberman.client.model.BombMap.Cell;
@@ -33,11 +33,15 @@ public class MapJFrame extends JFrame implements IView{
     private final Color BOMB_COLOR = Color.BLACK;
     private final Color EXPLODE_COLOR = Color.RED;
     private final Color BEAM_COLOR = Color.ORANGE;
+    private final String BOMB_ICON_PATH = "org/amse/bomberman/client/icons/bomb2.png";
+    private final String WALL_ICON_PATH = "org/amse/bomberman/client/icons/wall_blue-48.png";
+    private final String MAN_ICON_PATH = "org/amse/bomberman/client/icons/superman-48.png";
+    private final String BURN_ICON_PATH = "org/amse/bomberman/client/icons/burn-48.png";
     // is really nead???
     private boolean dead = false;
     private MapJFrameListener listener = new MapJFrameListener();
-    private final int height = 700;
-    private final int width = 650;
+    private final int height = 600;
+    private final int width = 500;
     
     public MapJFrame(BombMap map) {
         super("BomberMan");
@@ -58,7 +62,7 @@ public class MapJFrame extends JFrame implements IView{
         cells = new MyJPanel[num][num];
         for (int i = 0; i < num; i++) {
             for (int j = 0; j < num; j++) {
-                cells[i][j] = new MyJPanel(size);
+                cells[i][j] = new MyJPanel(48);
                 c.add(cells[i][j]);
             }
         }
@@ -105,29 +109,36 @@ public class MapJFrame extends JFrame implements IView{
             cells[x][y].checkExplosion(map.getValue(x, y));
         }
     }
-    private class MyJPanel extends JPanel {
+    private class MyJPanel extends JLabel {
         public MyJPanel(int size) {
             this.setPreferredSize(new Dimension(size, size));
         }
         public void setContent(int key) {
-            Color value = null;
+            Color color = EMPTY_COLOR;
+            ImageIcon icon = null;
+            ClassLoader cl = Main.class.getClassLoader();
             switch (key) {
                 case BombMap.EMPTY: { 
-                    value = EMPTY_COLOR;
-                    this.setBorder(new LineBorder(EMPTY_COLOR));
+                    icon = null;
                     break;
                 }
-                case BombMap.BOMB: { value = BOMB_COLOR; break;}
-                case BombMap.EXPLODED_BOMB: { value = EXPLODE_COLOR; break;}
-                case BombMap.BOMB_BEAM: { value = BEAM_COLOR; break;}
+                case BombMap.BOMB: {
+                    icon = new ImageIcon(cl.getResource(BOMB_ICON_PATH));
+                    break;
+                }
+                case BombMap.EXPLODED_BOMB: {
+                    icon = new ImageIcon(cl.getResource(BURN_ICON_PATH));
+                    break;
+                }
+                //case BombMap.BOMB_BEAM: { value = BEAM_COLOR; break;}
             }
             if (key < BombMap.EMPTY && key >= BombMap.BOMB_PROOF_WALL) {
-                value = WALL_COLOR;
-                this.setBorder(new LineBorder(Color.BLACK));
+                color = WALL_COLOR;
+                icon = new ImageIcon(cl.getResource(WALL_ICON_PATH));
             } else {
                 if (key > BombMap.EMPTY && key <= BombMap.MAX_PLAYERS) {
-                    // only for 2 players yet
-                    if (key == 1) {
+                    icon = new ImageIcon(cl.getResource(MAN_ICON_PATH));
+                    /*if (key == 1) {
                         value = PLAYER1_COLOR;
                     } else {
                         if (key == 2) {
@@ -135,30 +146,38 @@ public class MapJFrame extends JFrame implements IView{
                         } else {
                             value = PLAYER3_COLOR;
                         }
-                    }
+                    }*/
                 }
             }
-            this.setBackground(value);
+            this.setBackground(color);
+            this.setIcon(icon);
+            this.setOpaque(true);
         }
         public void checkExplosion(int mapValue){
-            Color value = null;
+            Color value = EMPTY_COLOR;
+            ImageIcon icon = null;
+            ClassLoader cl = Main.class.getClassLoader();
             // if it is wall
             if (mapValue < BombMap.EMPTY && mapValue >= BombMap.BOMB_PROOF_WALL) {
-                value = WALL_EXPL_COLOR;
+                //value = WALL_EXPL_COLOR;
+                icon = new ImageIcon(cl.getResource(BURN_ICON_PATH));
             } else {
                 // if it is player
                 if (mapValue > BombMap.EMPTY && mapValue <= BombMap.MAX_PLAYERS) {
+                    icon = new ImageIcon(cl.getResource(MAN_ICON_PATH));
                     value = PL_EXPL_COLOR;
                 } else {
                     // if it is center of Explosion
+                    icon = new ImageIcon(cl.getResource(BURN_ICON_PATH));
                     if (mapValue == BombMap.EXPLODED_BOMB) {
                         
                     } else {
-                        value = BEAM_COLOR;
+                        //value = BEAM_COLOR;
                     }
                 }
             }
             this.setBackground(value);
+            this.setIcon(icon);
         }
     }
 }
