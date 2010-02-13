817929006576a3a59dcd4e1b566d9866e9d96413
KirilchukVadim <KirilchukVadim@c20015b8-d073-11de-97bf-6350044336de>
2010-02-13 19:09:32 +0000

    Hard refactoring in ServerFrame. A little changes in IServer and Server, now server log is public and all exceptions more detalized.

--- src/org/amse/bomberman/server/ServerFrame.java
+++ src/org/amse/bomberman/server/ServerFrame.java
@@ -7,8 +7,6 @@ package org.amse.bomberman.server;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.HeadlessException;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -16,48 +14,38 @@ import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import org.amse.bomberman.server.net.IServer;
 import org.amse.bomberman.server.net.tcpimpl.Server;
 import org.amse.bomberman.util.Constants;
-import org.amse.bomberman.util.impl.ConsoleLog;
 
 /**
  * Server startup window.
  * @author Kirilchuck V.E.
  */
 public class ServerFrame extends JFrame {
+    private static final long serialVersionUID = 1L;
 
-    private int port;
+    private final String BTN_TEXT_RAISE = "Raise";
+    private final String BTN_TEXT_DOWN = "Down";
+    
     private IServer server;
     private JLabel portLabel = new JLabel("Server Port");
     private JTextField portField = new JTextField("" + Constants.DEFAULT_PORT);
-    private AbstractButton startButton = new JButton("Raise");
-    private AbstractButton stopButton = new JButton("Down");
-
-    private ConsoleLog log = new ConsoleLog();
+    private AbstractButton btnControl = new JButton(BTN_TEXT_RAISE);
+    private AbstractButton btnStatus = new JButton("Status");
 
     /** Creates new form ServerFrame */
-    public ServerFrame(){
+    public ServerFrame() {
 
         /*initial form properties*/
         this.setTitle("server control");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setMinimumSize(new Dimension(200, 100));
         this.setResizable(false);
-
-        /*setting relative(from cur screen resolution) appearing position*/
-        int screenHeight = 0;
-        int screenWidth = 0;
-        try {
-           Dimension screenDim = Toolkit.getDefaultToolkit().getScreenSize();
-           screenHeight = screenDim.height;
-           screenWidth = screenDim.width;
-        } catch (HeadlessException ex) {
-            log.println(ex.getMessage());
-        }
-        this.setLocation(screenHeight/2, screenWidth/2);
+        this.setLocationRelativeTo(null); //null for center position
 
         /*setting main layout of server control JFrame*/
         this.setLayout(new GridLayout(2, 1));
@@ -68,78 +56,92 @@ public class ServerFrame extends JFrame {
         portPanel.add(portField);
         this.add(portPanel);
 
-        /*server raise and server shutdown buttons*/
+        /*server control and status buttons*/
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout());
-        buttonPanel.add(startButton);
-        stopButton.setEnabled(false);
-        buttonPanel.add(stopButton);
+        buttonPanel.add(btnControl);
+        btnStatus.setEnabled(false);
+        buttonPanel.add(btnStatus);
         this.add(buttonPanel);
 
-        /*raise server button actionListener*/
-        startButton.addActionListener(new ActionListener() {
+        /*server control button actionListener*/
+        btnControl.addActionListener(new ServerControlButtonListener());
 
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    port = Integer.parseInt(portField.getText()); //throws NumberFormatException
-
-                    if (port < 0 || port > 65535) {
-                        throw new NumberFormatException(); //catching
-                    }
-                    
-                    if (server == null) { //if it is first initialization
-                        server = new Server(port);
-                    } else if (server.getPort() != port) { //if we want to raise server on new port
-                        if (!server.isShutdowned()) {
-                            server.shutdown();
-                        }
-                        log.println("Raising server on new port");
-                        server = new Server(port);
-                    }
-                    server.start();
-                } catch (NumberFormatException ex) { //parse errors                    
-                    //portField.setText("Error");
-                    log.println(ex.getMessage() + 
-                            "Must be int from 0 to 65535(inclusive)");
-                    return;
-                } catch (IOException ex) { //sockets errors. Server logging them.
-                    //portField.setText("Error");
-                    //log.println(ex.getMessage());
-                    return;
-                } catch (IllegalStateException ex) {
-                    //portField.setText("Error");
-                    log.println(ex.getMessage());
-                    return;
-                }
-                startButton.setEnabled(false);
-                stopButton.setEnabled(true);
-            }
-        });
-
-        /*shutdown server button actionListener*/
-        stopButton.addActionListener(new ActionListener() {
-
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    if (server != null) {
-                        server.shutdown();
-                    } else {
-                        throw new IllegalStateException("No server. Can`t shutdown."); //catching
-                    }
-                } catch (IOException ex) { //Server loggs IOErrors and throw them again.
-                    //portField.setText("Error");
-                    //log.println(ex.getMessage());
-                    return;
-                } catch (IllegalStateException ex) {
-                    //portField.setText("Error");
-                    log.println(ex.getMessage());
-                    return;
-                }
-                startButton.setEnabled(true);
-                stopButton.setEnabled(false);
-            }
-        });
+        /*server status button actionListener*/
+        btnStatus.addActionListener(new StatusButtonListener());
 
         this.setVisible(true);
+    }
+
+    private class ServerControlButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            if (btnControl.getText().equals(BTN_TEXT_RAISE)) {
+                raiseServer();
+            } else {
+                downServer();
+            }
+        }
+    }
+
+    private class StatusButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+          //TO DO
+        }
+    }
+
+    private void raiseServer() {
+        try {
+            int port = Integer.parseInt(portField.getText()); //throws NumberFormatException
+
+            if (port < 0 || port > 65535) {
+                throw new NumberFormatException(); //catching
+            }
+
+            if (server == null) { //if it is first initialization
+                server = new Server(port);
+            } else if (server.getPort() != port) { //if we want to raise server on new port
+                server.writeToLog("Raising server on new port");
+                server = new Server(port);
+            }
+            server.start();
+        } catch (NumberFormatException ex) { //parse errors
+            showErrorMessage(ex.getMessage());
+            return;
+        } catch (IOException ex) { //sockets errors. Server logging them.
+            showErrorMessage(ex.getMessage());
+            return;
+        } catch (IllegalStateException ex) {//must never happen
+            showErrorMessage(ex.getMessage());
+            return;
+        }
+
+        btnStatus.setEnabled(true);
+        btnControl.setText(BTN_TEXT_DOWN);
+    }
+
+    private void downServer() {
+        try {
+            if (server != null) {
+                server.shutdown();
+            } else {
+                String message = "No server. Can`t shutdown";
+                showErrorMessage(message);
+            }
+        } catch (IOException ex) { //Server loggs IOErrors and throw them again.
+            showErrorMessage(ex.getMessage());
+            return;
+        } catch (IllegalStateException ex) {//must never happen
+            showErrorMessage(ex.getMessage());
+            return;
+        }
+
+        btnStatus.setEnabled(false);
+        btnControl.setText(BTN_TEXT_RAISE);
+    }
+
+    private void showErrorMessage(String message) {
+        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
     }
 }
--- src/org/amse/bomberman/server/net/tcpimpl/Server.java
+++ src/org/amse/bomberman/server/net/tcpimpl/Server.java
@@ -23,7 +23,7 @@ import org.amse.bomberman.util.impl.ConsoleLog;
  *
  * @author Kirilchuk V.E.
  */
-public class Server implements IServer{
+public class Server implements IServer {
 
     private ILog log; // could be never initialized. Use writeToLog(...) instead of log.println(...)
     private int port;
@@ -31,18 +31,17 @@ public class Server implements IServer{
     private ServerSocket serverSocket;
     private boolean shutdowned = true; //true until we start accepting clients.
     private Thread listeningThread;
-    private int sessionCounter = 0; //need to generate name of log files.
+    private int sessionCounter = 0; //need to generate name of log files.   
 
     /**
      * Constructor with default port.
      */
     public Server() {
-        this.port = Constants.DEFAULT_PORT;
-        this.games = Collections.synchronizedList(new LinkedList<Game>());
+        this(Constants.DEFAULT_PORT);
     }
 
     /**
-     * Constructor that creating Server object with port param.
+     * Main constructor that creating Server object with port param.
      * @param port Free port number. Port must be between 0 and 65535, inclusive.
      * '0' for random free port. 0 is not reccomended cause clients must know
      * actual port number to connect.
@@ -50,37 +49,38 @@ public class Server implements IServer{
     public Server(int port) {
         this.port = port;
         this.games = Collections.synchronizedList(new LinkedList<Game>());
+        this.log = new ConsoleLog();
     }
 
     /**
      * Starts listening thread where ServerSocket.accept() method is using.
      * 
      */
-    public void start() throws IOException, IllegalStateException {
+    public synchronized void start() throws IOException, IllegalStateException {
         try {
 
             if (shutdowned) {
-                this.shutdowned = false;
                 this.sessionCounter = 0;
                 this.serverSocket = new ServerSocket(port, 0); // throws IOExeption,SecurityException
                 this.listeningThread = new Thread(new SocketListen(this));
                 this.listeningThread.start();
                 this.games = Collections.synchronizedList(new LinkedList<Game>());
-                this.log = new ConsoleLog();
+                this.shutdowned = false;
             } else {
                 throw new IllegalStateException("Already accepting. Can`t raise server.");
             }
 
         } catch (IOException ex) {
-            writeToLog(ex.getMessage());
-            this.shutdown();
+            writeToLog("Raise server error. " + ex.getMessage());
+//            this.shutdown();
             throw ex;
         } catch (SecurityException ex) {
-            writeToLog(ex.getMessage());
-            this.shutdown();
+            writeToLog("Raise server error. " + ex.getMessage());
+//            this.shutdown();
             throw ex;
         }
 
+        writeToLog("Server: started.");
     }
 
     /**
@@ -89,13 +89,12 @@ public class Server implements IServer{
      * @throws java.io.IOException if an error occurs when closing socket.
      * @throws IllegalStateException if we are trying to shutdown not raised server.
      */
-    public void shutdown() throws IOException, IllegalStateException {
+    public synchronized void shutdown() throws IOException, IllegalStateException {
         try {
 
-            if (!this.shutdowned) {
-                this.shutdowned = true;
+            if (!this.shutdowned) {            
                 if (this.listeningThread != null) {
-                    this.listeningThread.interrupt();
+                    this.listeningThread.interrupt(); //throws SecurityException
                     this.listeningThread = null;
                 }
                 if (this.serverSocket != null) {
@@ -103,20 +102,24 @@ public class Server implements IServer{
                     this.serverSocket = null;
                 }
                 this.games = null;
+                this.shutdowned = true;
                 if (this.log != null) {
-                    this.log.close();
+                    try{
+                        this.log.close();
+                    } catch (IOException ex){
+                        System.out.println("Shutdown server warning. Can`t close log." +
+                                ex.getMessage());
+                    }
                 }
             } else {
                 throw new IllegalStateException("Server is not raised. Can`t shutdown it.");
             }
 
         } catch (IOException ex) {
-            writeToLog("IOException in stopAcceptingClients() "
-                    + ex.getMessage());
+            writeToLog("Shutdown server error. " + ex.getMessage());
             throw ex;
         } catch (SecurityException ex) {
-            writeToLog("Thread can`t interrupt sessions by" +
-                    "security reasons in stopAcceptingClients(). " +
+            writeToLog("Shutdown server error.  " +
                     ex.getMessage());
             throw ex;
         }
@@ -125,32 +128,38 @@ public class Server implements IServer{
     }
 
     public void addGame(Game game) {
-        if(!this.shutdowned){
+        if (!this.shutdowned) { // is it redundant?
             this.games.add(game);
             writeToLog("Game added.");
-        }else{
-            writeToLog("Tryed to add game to shutdowned server.");
-            return;
+        } else {
+            writeToLog("Tryed to add game to shutdowned server.");            
         }
     }
 
     public void removeGame(Game gameToRemove) {
-        if(!this.shutdowned){
-           this.games.remove(gameToRemove);
-           writeToLog("Game removed");
-        }else{
-            writeToLog("Tryed to remove game from shutdowned server.");
-            return;
+        if (!this.shutdowned) { // is it redundant?
+            if(this.games.remove(gameToRemove)){
+                writeToLog("Game removed");
+            }else{
+                writeToLog("Server removeGame warning. No specified game found.");
+            }
+        } else {
+            writeToLog("Tryed to remove game from shutdowned server.");            
         }
     }
 
+    /**
+     * Return game from list at the specified index
+     * @param n
+     * @return
+     */
     public Game getGame(int n) {
         Game game = null;
-        if (!this.shutdowned) {
+        if (!this.shutdowned) { // is it redundant?
             try {
                 game = this.games.get(n);
             } catch (IndexOutOfBoundsException ex) {
-                writeToLog("Client tryed to get game with illegal ID. Canceled.");
+                writeToLog("Server getGame warning. Tryed to get game with illegal ID. Canceled.");
             }
         } else {
             writeToLog("Tryed to get game from shutdowned server.");
@@ -159,19 +168,19 @@ public class Server implements IServer{
     }
 
     public List<Game> getGamesList() {
-        if (!this.shutdowned){
+        if (!this.shutdowned) { // is it redundant?
             return this.games;
-        }else{ //if server was stopped.
-            writeToLog("Tryed to get games list from shutdowned server.");
+        } else { //if server was stopped.
+            writeToLog("Server getGamesList warning. Tryed to get games list from shutdowned server.");
             return null;
         }
     }
 
-    public boolean isShutdowned() {
+    public synchronized boolean isShutdowned() {
         return this.shutdowned;
     }
 
-    public int getPort() {
+    public synchronized int getPort() {
         return this.port;
     }
 
@@ -186,8 +195,7 @@ public class Server implements IServer{
         }
 
         public void run() {
-
-            writeToLog("Server: started.");
+            
             writeToLog("Server: Waiting for a new client...");
 
             try {
@@ -206,13 +214,13 @@ public class Server implements IServer{
                 }
 
             } catch (SocketTimeoutException ex) { //never happen in current realization
-                writeToLog(ex.getMessage());
+                writeToLog("Server error." + ex.getMessage());
             } catch (IOException ex) { //if an I/O error occurs when waiting for a connection.
-                writeToLog(ex.getMessage()); //or socket closed
+                writeToLog("Server error." + ex.getMessage()); //or socket closed
             } catch (SecurityException ex) { //accept wasn`t allowed
-                writeToLog(ex.getMessage());
+                writeToLog("Server error." + ex.getMessage());
             } catch (IllegalBlockingModeException ex) { //CHECK < THIS// what comments should i write?
-                writeToLog(ex.getMessage());
+                writeToLog("Server error." + ex.getMessage());
             }
 
             /*must free resources and stop our thread.*/
@@ -224,7 +232,7 @@ public class Server implements IServer{
         }
     }
 
-    private void writeToLog(String message) {
+    public void writeToLog(String message) {
         if (log == null) {
             System.out.println(message);
         } else {
