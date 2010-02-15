e5eb9365ec4af9d1afaa662042eff62a31d2a2f1
maverick89@bk.ru <maverick89@bk.ru@c20015b8-d073-11de-97bf-6350044336de>
2010-02-15 17:43:24 +0000

    Split class Connector and Object Serialization.

--- trunk/src/org/amse/bomberman/client/model/Bot.java
+++ trunk/src/org/amse/bomberman/client/model/Bot.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Random;
-import org.amse.bomberman.client.net.Connector;
+import org.amse.bomberman.client.net.impl.Connector;
 import org.amse.bomberman.client.net.IConnector;
 import org.amse.bomberman.util.*;
 import org.amse.bomberman.util.Constants.Direction;
