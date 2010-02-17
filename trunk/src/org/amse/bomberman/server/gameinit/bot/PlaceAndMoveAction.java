afa02ad9ee05db9ab6f6bcd418c77d43cc528641
KirilchukVadim <KirilchukVadim@c20015b8-d073-11de-97bf-6350044336de>
2010-02-17 22:21:38 +0000

    see last commit. Somehow two classes wasn`t commited...commiting them.

---
+++ trunk/src/org/amse/bomberman/server/gameinit/bot/PlaceAndMoveAction.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.amse.bomberman.server.gameinit.bot;
+
+import org.amse.bomberman.server.gameinit.imodel.IModel;
+import org.amse.bomberman.util.Constants.Direction;
+
+/**
+ *
+ * @author chibis
+ */
+public class PlaceAndMoveAction implements IAction{
+
+    private Direction direction;
+    private Bot bot;
+
+    public PlaceAndMoveAction(Direction direction, Bot bot) {
+        this.direction = direction;
+        this.bot = bot;
+    }
+
+    public void executeAction(IModel model) {
+        model.placeBomb(bot);
+        model.doMove(this.bot, this.direction);
+    }
+}
---
+++ trunk/src/org/amse/bomberman/server/gameinit/bot/RandomFullBotStrategy.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.amse.bomberman.server.gameinit.bot;
+
+import java.util.Random;
+import org.amse.bomberman.server.gameinit.GameMap;
+import org.amse.bomberman.server.gameinit.Pair;
+import org.amse.bomberman.server.gameinit.imodel.IModel;
+import org.amse.bomberman.util.Constants.Direction;
+
+/**
+ *
+ * @author chibis
+ */
+public class RandomFullBotStrategy extends BotStrategy{
+
+    Pair target;
+
+    private Pair findNewTarget(IModel model) {
+        GameMap map = model.getMap();
+        Random random = new Random();
+        int x = 0;
+        int y = 0;
+        do {
+            x = random.nextInt(map.getDimension() - 1);
+            y = random.nextInt(map.getDimension() - 1);
+        } while (!map.isEmpty(x, y));
+
+        Pair dir = new Pair(x, y);
+        //System.out.println("Choosed " + dir.toString());
+        return dir;
+    }
+
+    @Override
+    public IAction thinkAction(Bot bot, IModel model) {
+        if (bot.getPosition().equals(this.target) || target == null) {
+            target = findNewTarget(model);
+        }
+
+        Direction direction = null;
+        do {
+            try {
+                Thread.sleep(75);
+                direction = findWay(bot.getPosition(), target, model.getMapArray());
+                //System.out.println("Direction" + direction.toString());
+            } catch (IllegalArgumentException ex) {
+                target = findNewTarget(model);
+                //System.out.println("NEW TARGET");
+            } catch (InterruptedException ex){
+                ; //TO DO
+            }
+        } while (direction == null);
+
+        Random rnd = new Random();
+        int n = rnd.nextInt(100);
+
+        if(n<10){
+            return new PlaceAndMoveAction(direction, bot);
+        }
+        
+        return new MoveAction(direction, bot);
+    }
+}
