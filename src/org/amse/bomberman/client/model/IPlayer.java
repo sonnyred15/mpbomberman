74721d8bd99d1e9069ad72038c9d47b0e8466c70
maverick89@bk.ru <maverick89@bk.ru@c20015b8-d073-11de-97bf-6350044336de>
2010-02-20 00:06:47 +0000

    add files - IPlayer and MapJFrameUtil

---
+++ src/org/amse/bomberman/client/model/IPlayer.java
@@ -0,0 +1,15 @@
+package org.amse.bomberman.client.model;
+/**
+ *
+ * @author Michail Korovkin
+ */
+public interface IPlayer {
+    public void setName(String string);
+    public String getName();
+    public void setBombAmount(int amount);
+    public int getBombAmount();
+    public void setLives(int lives);
+    public int getLife();
+    public void setCoord(Cell cell);
+    public Cell getCoord();
+}
