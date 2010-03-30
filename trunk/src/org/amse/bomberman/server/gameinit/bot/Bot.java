88ed0bd373463f68d50caa50b10c3d3383b3b318
KirilchukVadim <KirilchukVadim@c20015b8-d073-11de-97bf-6350044336de>
2010-03-30 20:16:57 +0000

    added random delay to bots now they do move every 150-180ms

--- trunk/src/org/amse/bomberman/server/gameinit/bot/Bot.java
+++ trunk/src/org/amse/bomberman/server/gameinit/bot/Bot.java
@@ -2,6 +2,7 @@ package org.amse.bomberman.server.gameinit.bot;
 
 //~--- non-JDK imports --------------------------------------------------------
 
+import java.util.Random;
 import org.amse.bomberman.server.gameinit.Game;
 import org.amse.bomberman.server.gameinit.Player;
 import org.amse.bomberman.server.gameinit.control.GameEndedListener;
@@ -16,6 +17,7 @@ import org.amse.bomberman.server.gameinit.imodel.IModel;
 public class Bot extends Player
         implements GameStartedListener, GameEndedListener {
     private static final long BOT_STEP_DELAY = 150L;
+    private static final Random random = new Random();
     private boolean           gameEnded = false;
     private final Thread      botThread;
     private final Game        game;
@@ -51,7 +53,7 @@ public class Bot extends Player
                     IAction action = strategy.thinkAction(this.parent, model);
 
                     action.executeAction(model);
-                    Thread.sleep(Bot.BOT_STEP_DELAY);
+                    Thread.sleep(Bot.BOT_STEP_DELAY+random.nextInt(30));
                 } catch (InterruptedException ex) {
                     System.out.println("INTERRUPTED EXCEPTION IN BOT THREAD!!!!");
                 } catch (UnsupportedOperationException ex) {
