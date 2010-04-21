3f66424bee45114001498a4bdbe65621a1809373
KirilchukVadim <KirilchukVadim@c20015b8-d073-11de-97bf-6350044336de>
2010-04-21 20:15:35 +0000

    WPanel3 improvements(still not fixed width of leftPanel)

--- trunk/src/org/amse/bomberman/client/view/mywizard/WPanel3.java
+++ trunk/src/org/amse/bomberman/client/view/mywizard/WPanel3.java
@@ -1,9 +1,16 @@
 package org.amse.bomberman.client.view.mywizard;
 
+//~--- non-JDK imports --------------------------------------------------------
+
+import org.amse.bomberman.client.control.impl.Controller;
+import org.amse.bomberman.client.net.NetException;
+import org.amse.bomberman.util.Constants;
+import org.amse.bomberman.util.Creator;
+
+//~--- JDK imports ------------------------------------------------------------
+
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -13,17 +20,22 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+
 import java.io.IOException;
+
 import java.net.URL;
+
 import java.util.List;
+
 import javax.imageio.ImageIO;
+
 import javax.swing.AbstractAction;
 import javax.swing.Box;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -32,37 +44,43 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.border.EmptyBorder;
-import org.amse.bomberman.client.net.NetException;
-import org.amse.bomberman.client.control.impl.Controller;
-import org.amse.bomberman.util.Constants;
-import org.amse.bomberman.util.Creator;
+
 /**
  *
  * @author Michael Korovkin
+ * @author Kirilchuk Vadim
  */
-public class WPanel3 extends JPanel{
+public class WPanel3 extends JPanel {
     private final static long serialVersionUID = 1L;
-    private final int         height = 480;
+
+    //
+    private final int width = 640;
+    private final int height = 480;
 
     //
     private int              maxPlayers = Constants.MAX_PLAYERS;
-    private final int        width = 640;
     private final JTextField messageTF = new JTextField();
+    private final JComponent playersPanel;
 
     //
-    private final JTextArea chatTA = new JTextArea();
-    private final JButton   chatJButton = new JButton("Send");
-    private final JButton   botRemoveJButton = new JButton("Remove");
+    private final JTextArea  chatTA = new JTextArea();
+    private final JButton    chatJButton = new JButton("Send");
+    private final JComponent chatPanel;
 
     //
-    private JButton botAddJButton = new JButton("Add bot");
+    private JButton          botAddJButton = new JButton("Add bot");
+    private final JButton    botRemoveJButton = new JButton("Remove");
+    private final JComponent botsPanel;
 
     //
-    private JList    playersList;
+    private JList playersList;
 
     public WPanel3() {
         super();
         this.setSize(width, height);
+        this.playersPanel = this.createPlayersPanel();
+        this.botsPanel = this.createBotsPanel();
+        this.chatPanel = this.createChatPanel();
         initComponents();
         setVisible(true);
     }
@@ -73,14 +91,12 @@ public class WPanel3 extends JPanel{
 
     public void setGameInfo(List<String> info) {
         if (info.get(0).equals("false")) {
-            botAddJButton.setEnabled(false);
-            botAddJButton.setVisible(false);
-            botRemoveJButton.setEnabled(false);
-            botRemoveJButton.setVisible(false);
+            this.botsPanel.setVisible(false);
+
             // TODO
-            //wizard.setNextButtonEnable(false);
+            // wizard.setNextButtonEnable(false);
         } else {
-            botAddJButton.setAction(new AddBotAction());
+            this.botsPanel.setVisible(true);
         }
 
         maxPlayers = Integer.parseInt(info.get(1));
@@ -111,18 +127,32 @@ public class WPanel3 extends JPanel{
         this.setLayout(new BorderLayout(10, 0));
 
         /* creating players and botsControl panels */
-        JPanel playersPanel = this.createPlayersPanel();
-        JPanel botsPanel = this.createBotsPanel();
-        Box    box = Box.createVerticalBox();
+        JPanel left = new JPanel(new GridBagLayout());
 
-        box.add(playersPanel);
-        box.add(botsPanel);
-        box.add(Box.createHorizontalStrut(190));    // TODO bad decisont to use magic 190
-        this.add(box, BorderLayout.WEST);
+        left.setBorder(new EmptyBorder(0, 5, 5, 0));
 
-        /* creating and adding chat panel in the center */
-        JPanel chatPanel = this.createChatPanel();
+        //
+        GridBagConstraints cons = new GridBagConstraints();
 
+        cons.anchor = GridBagConstraints.NORTHWEST;
+        cons.fill = GridBagConstraints.BOTH;
+        cons.gridx = 0;
+        cons.gridy = 0;
+        cons.weightx = 1;
+        cons.weighty = 1;
+        left.add(playersPanel, cons);
+
+        //
+        cons = new GridBagConstraints();
+        cons.anchor = GridBagConstraints.PAGE_END;
+        cons.gridx = 0;
+        cons.gridy = 1;
+        left.add(botsPanel, cons);
+
+        //
+        this.add(left, BorderLayout.WEST);
+
+        /* adding chat panel in the center */
         this.add(chatPanel, BorderLayout.CENTER);
         this.add(Box.createVerticalStrut(10), BorderLayout.NORTH);
         messageTF.addKeyListener(new KeyAdapter() {
@@ -139,14 +169,14 @@ public class WPanel3 extends JPanel{
                         } catch (NetException ex) {
 
                             // is it good?
-                            ex.printStackTrace();//TO DO
-                            //wizard.setCurrentJPanel(0);
+                            ex.printStackTrace();    // TO DO
+
+                            // wizard.setCurrentJPanel(0);
                         }
                     }
                 }
             }
         });
-        chatJButton.setAction(new ChatAction());
     }
 
     private JPanel createPlayersPanel() {
@@ -155,19 +185,28 @@ public class WPanel3 extends JPanel{
         this.playersList = new JList(defaultListModel);
         this.playersList.setCellRenderer(new IconListRenderer());
 
-        JPanel      playersPanel = new JPanel(new GridLayout());
-        JScrollPane scroll = new JScrollPane(playersPanel);
+        JPanel      playersJPanel = new JPanel(new GridLayout());
+        JScrollPane scrollJList = new JScrollPane(playersList);
 
-        scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-        scroll.setWheelScrollingEnabled(true);
-        playersPanel.add(new JScrollPane(this.playersList));
-        playersPanel.setBorder(new EmptyBorder(0, 10, 0, 0));
+        scrollJList.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollJList.setWheelScrollingEnabled(true);
+        playersJPanel.add(scrollJList);
 
-        return playersPanel;
+        return playersJPanel;
+    }
+
+    private JPanel createBotsPanel() {
+        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+        panel.add(botAddJButton);
+        this.botAddJButton.setAction(new AddBotAction());
+        panel.add(botRemoveJButton);
+
+        return panel;
     }
 
     private JPanel createChatPanel() {
-        JPanel             chatPanel = new JPanel(new GridBagLayout());
+        JPanel             chatJPanel = new JPanel(new GridBagLayout());
         GridBagConstraints cons = new GridBagConstraints();
 
         this.chatTA.setEditable(false);
@@ -181,7 +220,7 @@ public class WPanel3 extends JPanel{
         cons.gridheight = 1;
         cons.fill = GridBagConstraints.BOTH;
         cons.insets = new Insets(0, 0, 5, 10);
-        chatPanel.add(new JScrollPane(chatTA), cons);
+        chatJPanel.add(new JScrollPane(chatTA), cons);
 
         // TEXT FIELD
         cons = new GridBagConstraints();
@@ -191,7 +230,7 @@ public class WPanel3 extends JPanel{
         cons.gridwidth = GridBagConstraints.RELATIVE;
         cons.fill = GridBagConstraints.HORIZONTAL;
         cons.insets = new Insets(0, 0, 5, 10);
-        chatPanel.add(this.messageTF, cons);
+        chatJPanel.add(this.messageTF, cons);
 
         // SEND BUTTON
         cons = new GridBagConstraints();
@@ -200,9 +239,10 @@ public class WPanel3 extends JPanel{
         cons.gridwidth = GridBagConstraints.REMAINDER;
         cons.fill = GridBagConstraints.NONE;
         cons.insets = new Insets(0, 0, 5, 10);
-        chatPanel.add(this.chatJButton, cons);
+        this.chatJButton.setAction(new ChatAction());
+        chatJPanel.add(this.chatJButton, cons);
 
-        return chatPanel;
+        return chatJPanel;
     }
 
     private String getMessage() {
@@ -211,16 +251,6 @@ public class WPanel3 extends JPanel{
         messageTF.setText("");
 
         return message;
-    }
-
-    private JPanel createBotsPanel() {
-        JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER, 5, 5));
-
-        panel.add(botAddJButton);
-        panel.add(botRemoveJButton);
-        panel.setBorder(new EmptyBorder(0, 10, 0, 0));
-
-        return panel;
     }
 
     private class AddBotAction extends AbstractAction {
@@ -236,11 +266,15 @@ public class WPanel3 extends JPanel{
             try {
                 Controller.getInstance().requestJoinBotIntoGame();
             } catch (NetException ex) {
+
                 // TO DO
-                /*JOptionPane.showMessageDialog(wizard,
-                        "Connection was lost.\n" + ex.getMessage(), "Error",
-                        JOptionPane.ERROR_MESSAGE);
-                wizard.setCurrentJPanel(0);*/
+
+                /*
+                 * JOptionPane.showMessageDialog(wizard,
+                 *       "Connection was lost.\n" + ex.getMessage(), "Error",
+                 *       JOptionPane.ERROR_MESSAGE);
+                 * wizard.setCurrentJPanel(0);
+                 */
             }
         }
     }
@@ -262,12 +296,15 @@ public class WPanel3 extends JPanel{
                     Controller.getInstance().requestSendChatMessage(message);
                 }
             } catch (NetException ex) {
+
                 // TO DO
+
                 /*
-                JOptionPane.showMessageDialog(wizard,
-                        "Connection was lost.\n" + ex.getMessage(), "Error",
-                        JOptionPane.ERROR_MESSAGE);
-                wizard.setCurrentJPanel(0); */
+                 * JOptionPane.showMessageDialog(wizard,
+                 *       "Connection was lost.\n" + ex.getMessage(), "Error",
+                 *       JOptionPane.ERROR_MESSAGE);
+                 * wizard.setCurrentJPanel(0);
+                 */
             }
         }
     }
