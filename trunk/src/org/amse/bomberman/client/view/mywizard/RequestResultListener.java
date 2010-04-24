fd526d54ea0bc695a3e0d1ae4c9934d55eb16b28
maverick89@bk.ru <maverick89@bk.ru@c20015b8-d073-11de-97bf-6350044336de>
2010-04-24 00:10:45 +0000

    Some refactoring. Changes in packages-2.

--- trunk/src/org/amse/bomberman/client/view/mywizard/RequestResultListener.java
+++
@@ -1,8 +0,0 @@
-package org.amse.bomberman.client.view.mywizard;
-import java.util.List;
-/**
- * @author Kirilchuk V.E
- */
-public interface RequestResultListener {
-    void received(List<String> result);
-}
