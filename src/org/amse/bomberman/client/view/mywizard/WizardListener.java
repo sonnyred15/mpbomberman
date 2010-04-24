c1b55adf77d49ef972391d00be7366d36b19d3b7
maverick89@bk.ru <maverick89@bk.ru@c20015b8-d073-11de-97bf-6350044336de>
2010-04-24 00:10:45 +0000

    Some refactoring. Changes in packages-2.

--- src/org/amse/bomberman/client/view/mywizard/WizardListener.java
+++
@@ -1,9 +0,0 @@
-package org.amse.bomberman.client.view.mywizard;
-
-/**
- *
- * @author Michael Korovkin
- */
-public interface WizardListener {
-    public void wizardActionPerformed(WizardAction a);
-}
