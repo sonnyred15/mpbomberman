708dffdb4635fb2e73311711fecfbf9f9c0b3d0a
KirilchukVadim@gmail.com <KirilchukVadim@gmail.com@c20015b8-d073-11de-97bf-6350044336de>
2012-10-11 17:29:20 +0000

    removed obsolete branch

--- branches/NewProtocol/src/org/amse/bomberman/client/view/wizard/WizardListener.java
+++
@@ -1,9 +0,0 @@
-package org.amse.bomberman.client.view.wizard;
-
-/**
- *
- * @author Mikhail Korovkin
- */
-public interface WizardListener {
-    public void wizardActionPerformed(WizardEvent a);
-}
